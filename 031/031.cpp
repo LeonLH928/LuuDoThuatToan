0aaa13d12f4706b64cc79b6f23687e300737d5d5
ryan <phanhuynhhung0707@gmail.com>
2025-07-03 14:16:25 +0700

    3. finish 031

---
+++ 031/031.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	float n; cin >> n;
+	float s = 0;
+
+	for (int i = 1; i <= 2 * n + 1; i += 2)
+	{
+		s += 1.0 / i;
+	}
+
+	cout << "Gia tri cua s = " << s;
+		
+	cout << "\n\n\nKet thuc!!!!!\n\n\n";
+	return 0;
+}
