f8d3f4995d0d9a67cea31c86b77309a6586fcec7
AnhGam <guardalex8@gmail.com>
2025-07-03 13:51:19 +0700

    005

---
+++ 005/005.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float r;
+	cin >> r;
+	float pi = 3.14;
+	float v =4* pi * r * r / 3;
+	cout << v << endl;
+
+	cout << "\n\n\n Ket thuc !!!!!\n\n\n";
+	return 0;
+}
