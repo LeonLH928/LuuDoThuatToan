9c0a925075d619a6e6d6836cf9405f176ab9c6b1
Phan Long <phanmainhatlong@outlook.com>
2025-07-03 14:39:24 +0700

    100-2

--- 100/100.cpp
+++ 100/100.cpp
@@ -14,7 +14,10 @@ int main()
     while (i <= n)
     {
         t = t * i;
-        s = pow(s + t, i + 1);
+        s = pow(s + t, 1.0f / (i + 1));
         i = i + 1;
     }
+    
+    cout << s;
+    return 0;
 }
